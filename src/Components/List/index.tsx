2727826c88cf52ca3c95b59cb4fb214a62422715
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-07-10 14:51:34 -0300

    fix some console errors from console

--- src/Components/List/index.tsx
+++ src/Components/List/index.tsx
@@ -1,31 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function List() {
-  const subjects = [{
-    task: 'React',
-    time: '02:00:00'
-  },
-  {
-    task: 'Javascript',
-    time: '01:00:00'
-  },
-  {
-    task: 'Typescript',
-    time: '03:00:00'
-  }
-]
+  const subjects = [
+    {
+      task: "React",
+      time: "02:00:00",
+    },
+    {
+      task: "Javascript",
+      time: "01:00:00",
+    },
+    {
+      task: "Typescript",
+      time: "03:00:00",
+    },
+  ];
 
-  return(
+  return (
     <aside>
       <h2>Estudos do dia</h2>
       <ul>
-        {subjects.map((subject) => (
-          <li>
+        {subjects.map((subject, index) => (
+          <li key={index}>
             <h3>{subject.task}</h3>
             <span>{subject.time}</span>
           </li>
         ))}
       </ul>
     </aside>
-  )
+  );
 }
