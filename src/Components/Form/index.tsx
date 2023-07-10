f070b9137f5f62c2172099bba7f1f8976d9422bb
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-07-10 15:50:40 -0300

    add style to form component

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -1,35 +1,32 @@
-import React from 'react'
-import Button from '../Button'
+import React from "react";
+import Button from "../Button";
+import "./style.scss";
 
 export default function Form() {
-  return(
-    <form>
-      <div>
-        <label htmlFor='task'>
-          Adicione um novo estudo
-        </label>
+  return (
+    <form className="novaTarefa">
+      <div className="inputContainer">
+        <label htmlFor="task">Adicione um novo estudo</label>
         <input
-          id='task'
-          name='task'
+          id="task"
+          name="task"
           required
-          placeholder='O que voçê quer estudar ?'
-          type='text'
+          placeholder="O que voçê quer estudar ?"
+          type="text"
         />
       </div>
-      <div>
-        <label htmlFor='time'>
-          Tempo
-        </label>
+      <div className="inputContainer">
+        <label htmlFor="time">Tempo</label>
         <input
-          id='time'
-          name='time'
-          type='time'
+          id="time"
+          name="time"
+          type="time"
           required
-          min='00:00:00'
-          max='01:30:00'
+          min="00:00:00"
+          max="01:30:00"
         />
       </div>
-      <Button children='Button'/>
+      <Button children="Button" />
     </form>
-  )
+  );
 }
