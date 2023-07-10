c62eb6ca0a009a826c81b9503ef1697365f245aa
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-07-10 15:26:57 -0300

    add style to app component

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../Components/Form";
+import List from "../Components/List";
+import "./style.scss";
+
+export default function App() {
+  return (
+    <div className="AppStyle">
+      <Form />
+      <List />
+    </div>
+  );
+}
