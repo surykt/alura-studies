f7b795b50d0e5ed508351c73c902515b275f4213
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-07-11 18:17:39 -0300

    create states to form component

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import Button from "../Button";
 import style from "./Form.module.scss";
 
 export default function Form() {
+  const [task, setTask] = useState("");
+  const [time, setTime] = useState("00:00");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(task, time);
+  }
+
   return (
-    <form className={style.novaTarefa}>
+    <form className={style.novaTarefa} onSubmit={handleSubmit}>
       <div className={style.inputContainer}>
         <label htmlFor="task">Adicione um novo estudo</label>
         <input
           id="task"
           name="task"
+          value={task}
+          onChange={event => setTask(event.target.value)}
           required
           placeholder="O que voçê quer estudar ?"
           type="text"
@@ -20,6 +31,8 @@ export default function Form() {
           id="time"
           name="time"
           type="time"
+          value={time}
+          onChange={event => setTime(event.target.value)}
           required
           min="00:00:00"
           max="01:30:00"
