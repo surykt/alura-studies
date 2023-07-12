3a4999b465224bf67a0f5f6b875837890ba94a2b
Carlos Alan <cardosocarlosalan@gmail.com>
2023-07-11 21:53:23 -0300

    add type prop to button component

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -2,8 +2,9 @@ import style from "./Button.module.scss";
 
 interface ButtonProps {
   children: string;
+  type?: 'button' | 'submit' | 'reset'
 }
 
-export default function Button({ children }: ButtonProps) {
-  return <button className={style.botao}>{children}</button>;
+export default function Button({ children, type }: ButtonProps) {
+  return <button className={style.botao} type={type}>{children}</button>;
 }
